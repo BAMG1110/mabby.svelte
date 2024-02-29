63ff10397e09a72772c29aecda28f81555194209
mabby <rznns.mabby@gmail.com>
2024-02-29 16:32:10 -0600

    layout inicial. grid

--- mabby.svelte/src/routes/+layout.svelte
+++ mabby.svelte/src/routes/+layout.svelte
@@ -1,5 +1,46 @@
-<nav>
-    <a href="/">Macetas</a>
-    <a href="/about">about</a>
-</nav>
-<slot/>
+<script>
+	import logo from '$lib/assets/logo.gif';
+</script>
+
+<style>
+    img {
+        width: 4rem;
+        height: 4rem;
+        border-radius: 50%;
+    }
+
+    .main {
+        height: 100vh;
+        display: grid;
+        grid-template-columns: minmax(10rem, 2fr) minmax(auto, 10fr);
+        grid-auto-rows: 1fr 10fr 1fr;
+    }
+    footer {
+        grid-column-start: 1;
+        grid-column-end: -1;
+    }
+</style>
+
+<div class="main">
+    <div class="logo">
+        <a href="/">
+            <img src={logo} alt="mabby1110 logo">
+        </a>
+    </div>
+    <nav>
+        <div class="navbar">
+            <a href="/macetas">Macetas</a>
+            <a href="/about">about</a>
+        </div>
+    </nav>
+    <div class="sidebar">
+        sidebar
+    </div>
+    <div class="content">
+        contenido
+        <slot/>
+    </div>
+</div>
+<footer>
+    footer
+</footer>
--- mabby.svelte/src/routes/+page.svelte
+++ mabby.svelte/src/routes/+page.svelte
@@ -1,24 +1,6 @@
-<div class="parent">
-    <div class="child">
-        <h2>hola, soy </h2><h1>mabby</h1>
-    </div>
-    <div class="child">
-        jnfdsjf
-    </div>
-    <div class="child">
-        jnfdsjf
-    </div>
+<div>
+    <h2>hola, soy </h2><h1>mabby</h1>
 </div>
 
 <style>
-    .child {
-        background-color:lightslategrey;
-    }
-    .parent {
-        display: grid;
-        gap: 1rem;
-
-        grid-template-columns: repeat(auto-fit, minmax(15rem, auto));
-        grid-auto-rows: 28rem;
-    }
 </style>
